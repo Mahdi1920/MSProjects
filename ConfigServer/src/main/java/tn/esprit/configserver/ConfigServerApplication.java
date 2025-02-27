4fdee31c5209a3f7c9570869e91786d09df20e75
mahdi <mahdichabbouh98@gmail.com>
2025-02-27 01:11:07 +0100

    config server with private repository

--- ConfigServer/src/main/java/tn/esprit/configserver/ConfigServerApplication.java
+++ ConfigServer/src/main/java/tn/esprit/configserver/ConfigServerApplication.java
@@ -1,5 +1,6 @@
 package tn.esprit.configserver;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -9,6 +10,11 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("GIT_URI", dotenv.get("GIT_URI"));
+		System.setProperty("GIT_USERNAME", dotenv.get("GIT_USERNAME"));
+		System.setProperty("GIT_TOKEN", dotenv.get("GIT_TOKEN"));
+
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
