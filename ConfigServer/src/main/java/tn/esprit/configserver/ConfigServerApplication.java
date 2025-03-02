172336706c20b44553497847df1272706a8191e5
mahdi <mahdichabbouh98@gmail.com>
2025-03-02 01:16:09 +0100

    changes

--- ConfigServer/src/main/java/tn/esprit/configserver/ConfigServerApplication.java
+++ ConfigServer/src/main/java/tn/esprit/configserver/ConfigServerApplication.java
@@ -1,6 +1,5 @@
 package tn.esprit.configserver;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
